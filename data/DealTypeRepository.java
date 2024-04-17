ed20c51c3df56b9e8eb819d37d5ff151ff07f471
Maria <93052580+Maria007muse@users.noreply.github.com>
2024-04-17 17:09:50 +0300

    Delete data directory

--- data/DealTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package stores.dto;
-
-import org.springframework.data.repository.CrudRepository;
-import stores.DealType;
-
-public interface DealTypeRepository extends CrudRepository<DealType, Long> {
-}
--- data/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package stores.dto;
-import org.springframework.data.repository.CrudRepository;
-import stores.TacoOrder;
-public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
-}
-
